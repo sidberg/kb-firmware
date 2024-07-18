68ce33ef75587f8b4a4ef3af5e411430c5d58d15
Sidney Berg <sidneysea@gmail.com>
2024-07-18 09:57:34 -0700

    capsword for ortho40

--- ortho40/keymaps/default/keymap.c
+++ ortho40/keymaps/default/keymap.c
@@ -24,14 +24,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |------+------+------+------+------+------+------+------+------+------+------+------|
     * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
     * |------+------+------+------+------+------+------+------+------+------+------+------|
-    * | Ctrl |SupLow| Super| Alt  | Lower|    Space    |Raise | Left | Down |  Up  |Right |
+    * | Ctrl |CapWrd| Super| Alt  | Lower|    Space    |Raise | Left | Down |  Up  |Right |
     * `-----------------------------------------------------------------------------------'
     */
     [_BASE] = LAYOUT_grid(
-        KC_ESC,  KC_Q,                 KC_W,    KC_E,    KC_R,       KC_T,    KC_Y, KC_U,       KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_TAB,  KC_A,                 KC_S,    KC_D,    KC_F,       KC_G,    KC_H, KC_J,       KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,                 KC_X,    KC_C,    KC_V,       KC_B,    KC_N, KC_M,       KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-        KC_LCTL, LM(_LOWER, MOD_LGUI), KC_LGUI, KC_LALT, MO(_LOWER), KC_SPC,        MO(_RAISE), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,       KC_T,    KC_Y, KC_U,       KC_I,    KC_O,    KC_P,    KC_BSPC,
+        KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,       KC_G,    KC_H, KC_J,       KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,       KC_B,    KC_N, KC_M,       KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+        KC_LCTL, CW_TOGG, KC_LGUI, KC_LALT, MO(_LOWER), KC_SPC,        MO(_RAISE), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     ),
 
     /* Lower
