ef823449b91e41cb0ae28ab149d6c0786172216a
Sidney Berg <sidneysea@gmail.com>
2024-08-10 14:03:36 -0700

    dactyl: switch around thumb keys

--- dactyl/keymaps/default/keymap.c
+++ dactyl/keymaps/default/keymap.c
@@ -16,14 +16,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * +------+------+------+------+------+------|                                  |------+------+------+------+------+------|
      *        |   z  |   x  |   c  |   v  |   b  |                                  |   n  |   m  |   ,  |   .  |   /  |
      *        +------+------+------+-------------+------+------+      +------+------+------+------+------+------+------+
-     *                                    |lwrspc|ctldel|altesc|      |suptab|shftbk|uprent|
+     *                                    |ctlesc|lwrspc|suptab|      |shftbk|uprent|altdel|
      *                                    +------+------+------+      +------+------+------+
      */
     [_BASE] = LAYOUT(
                        KC_Q,     KC_W,   KC_E,     KC_R,    KC_T,         KC_Y,     KC_U,     KC_I,     KC_O,    KC_P,
         MO(_ADJUST),   KC_A,     KC_S,   KC_D,     KC_F,    KC_G,         KC_H,     KC_J,     KC_K,     KC_L,    KC_SCLN,   KC_QUOT,
                        KC_Z,     KC_X,   KC_C,     KC_V,    KC_B,         KC_N,     KC_M,     KC_COMM,  KC_DOT,  KC_SLSH,
-              LT(_LOWER, KC_SPC), LCTL_T(KC_DEL), LALT_T(KC_ESC),         LGUI_T(KC_TAB), RSFT_T(KC_BSPC), LT(_RAISE, KC_ENT)
+              LCTL_T(KC_ESC), LT(_LOWER, KC_SPC), LGUI_T(KC_TAB),         RSFT_T(KC_BSPC), LT(_RAISE, KC_ENT), RALT_T(KC_DEL)
     ),
 
     /* Lower
